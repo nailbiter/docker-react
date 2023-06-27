50f3a7bb93a62d959878c527e53aa3d6e8e5a573
Oleksii Leontiev <alozz1991@gmail.com>
2023-06-27 20:21:43 +0900

    fix tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,3 +6,6 @@ import App from './App';
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+test('adds 1 + 2 to equal 3', () => {
+  expect(1+2).toBe(3);
+});
