ccacc75d7143517e42663e891c2f9065520c8495
Oleksii Leontiev <alozz1991@gmail.com>
2023-08-20 00:00:28 +0900

    progress: 275

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,11 @@
 ## it's not entirely as easy as we'd thought
 
 ## 1.
-docker build -t nailbiter/multi-client -f ./client/Dockerfile ./client
-docker build -t nailbiter/multi-server -f ./server/Dockerfile ./server
-docker build -t nailbiter/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t nailbiter/multi-client:latest -t nailbiter/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t nailbiter/multi-server:latest -t nailbiter/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t nailbiter/multi-worker:latest -t nailbiter/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+## note: additional benifit of using $GIT_SHA -- simplifies debugging
 
 docker push nailbiter/multi-client
 docker push nailbiter/multi-server
@@ -29,4 +31,7 @@ kubectl apply -f k8s
 ## problem with 👆:
 ## * (we spoke about this before -- need to use imperative command)
 ## * ==> need to tag image with unique number
-## * we omitted it above
+## * we omitted it above (see history of this file)
+kubectl set image deployments/server-deployment server=nailbiter/multi-server:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=nailbiter/multi-worker:$GIT_SHA
+kubectl set image deployments/client-deployment client=nailbiter/multi-client:$GIT_SHA
