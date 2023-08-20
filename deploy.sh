4df899f6064828bd9ec13d34f35c952b2729547c
Oleksii Leontiev <alozz1991@gmail.com>
2023-08-20 10:51:54 +0900

    progress: 276

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@
 ## it's not entirely as easy as we'd thought
 
 ## 1.
-docker build -t nailbiter/multi-client:latest -t nailbiter/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
-docker build -t nailbiter/multi-server:latest -t nailbiter/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
-docker build -t nailbiter/multi-worker:latest -t nailbiter/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker build -t nailbiter/multi-client:latest -t nailbiter/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nailbiter/multi-server:latest -t nailbiter/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nailbiter/multi-worker:latest -t nailbiter/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-## note: additional benifit of using $GIT_SHA -- simplifies debugging
+## note: additional benifit of using $SHA -- simplifies debugging
 
 docker push nailbiter/multi-client
 docker push nailbiter/multi-server
@@ -30,8 +30,8 @@ kubectl apply -f k8s
 # > kubectl set image deployments/server-deployment server=nailbiter/multi-server
 ## problem with 👆:
 ## * (we spoke about this before -- need to use imperative command)
-## * ==> need to tag image with unique number
+## * ==> need to tag image with unique tag (will use sha)
 ## * we omitted it above (see history of this file)
-kubectl set image deployments/server-deployment server=nailbiter/multi-server:$GIT_SHA
-kubectl set image deployments/worker-deployment worker=nailbiter/multi-worker:$GIT_SHA
-kubectl set image deployments/client-deployment client=nailbiter/multi-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=nailbiter/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=nailbiter/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=nailbiter/multi-client:$SHA
