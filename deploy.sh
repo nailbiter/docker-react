a9db17861f9e08738e4ad414b57039fc9082b82d
Oleksii Leontiev <alozz1991@gmail.com>
2023-08-20 20:16:46 +0900

    stash

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t rallycoding/multi-client-k8s:latest -t rallycoding/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t rallycoding/multi-server-k8s:latest -t rallycoding/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
-docker build -t rallycoding/multi-worker-k8s:latest -t rallycoding/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t $DOCKER_ID/multi-client-k8s:latest -t $DOCKER_ID/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t $DOCKER_ID/multi-server-k8s:latest -t $DOCKER_ID/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
+docker build -t $DOCKER_ID/multi-worker-k8s:latest -t $DOCKER_ID/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push rallycoding/multi-client-k8s:latest
-docker push rallycoding/multi-server-k8s:latest
-docker push rallycoding/multi-worker-k8s:latest
+docker push $DOCKER_ID/multi-client-k8s:latest
+docker push $DOCKER_ID/multi-server-k8s:latest
+docker push $DOCKER_ID/multi-worker-k8s:latest
 
-docker push rallycoding/multi-client-k8s:$SHA
-docker push rallycoding/multi-server-k8s:$SHA
-docker push rallycoding/multi-worker-k8s:$SHA
+docker push $DOCKER_ID/multi-client-k8s:$SHA
+docker push $DOCKER_ID/multi-server-k8s:$SHA
+docker push $DOCKER_ID/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=rallycoding/multi-server-k8s:$SHA
-kubectl set image deployments/client-deployment client=rallycoding/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=rallycoding/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=$DOCKER_ID/multi-server-k8s:$SHA
+kubectl set image deployments/client-deployment client=$DOCKER_ID/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=$DOCKER_ID/multi-worker-k8s:$SHA
