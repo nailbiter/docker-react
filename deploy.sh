82c7ed4a0e9fc9d59dbf67ae293b4ddd1574a764
Oleksii Leontiev <alozz1991@gmail.com>
2023-08-22 20:36:07 +0900

    stash

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,21 @@
-docker build -t $DOCKER_ID/multi-client-k8s:latest -t $DOCKER_ID/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t $DOCKER_ID/multi-server-k8s:latest -t $DOCKER_ID/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
-docker build -t $DOCKER_ID/multi-worker-k8s:latest -t $DOCKER_ID/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+# docker build -t $DOCKER_ID/multi-client-k8s:latest -t $DOCKER_ID/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+# docker build -t $DOCKER_ID/multi-server-k8s:latest -t $DOCKER_ID/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
+# docker build -t $DOCKER_ID/multi-worker-k8s:latest -t $DOCKER_ID/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push $DOCKER_ID/multi-client-k8s:latest
-docker push $DOCKER_ID/multi-server-k8s:latest
-docker push $DOCKER_ID/multi-worker-k8s:latest
+# docker push $DOCKER_ID/multi-client-k8s:latest
+# docker push $DOCKER_ID/multi-server-k8s:latest
+# docker push $DOCKER_ID/multi-worker-k8s:latest
 
-docker push $DOCKER_ID/multi-client-k8s:$SHA
-docker push $DOCKER_ID/multi-server-k8s:$SHA
-docker push $DOCKER_ID/multi-worker-k8s:$SHA
+# docker push $DOCKER_ID/multi-client-k8s:$SHA
+# docker push $DOCKER_ID/multi-server-k8s:$SHA
+# docker push $DOCKER_ID/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=$DOCKER_ID/multi-server-k8s:$SHA
-kubectl set image deployments/client-deployment client=$DOCKER_ID/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=$DOCKER_ID/multi-worker-k8s:$SHA
+
+kubectl set image deployments/server-deployment server=$DOCKER_ID/multi-server-k8s:latest
+kubectl set image deployments/client-deployment client=$DOCKER_ID/multi-client-k8s:latest
+kubectl set image deployments/worker-deployment worker=$DOCKER_ID/multi-worker-k8s:latest
+
+# kubectl set image deployments/server-deployment server=$DOCKER_ID/multi-server-k8s:$SHA
+# kubectl set image deployments/client-deployment client=$DOCKER_ID/multi-client-k8s:$SHA
+# kubectl set image deployments/worker-deployment worker=$DOCKER_ID/multi-worker-k8s:$SHA
